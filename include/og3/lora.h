e0dc0217e34859e8a5c56ff77983280b2db30253
Chris Lee <lee.chris.h@gmail.com>
2025-06-06 14:40:30 -0400

    In development: options/variables support for Lora modules

--- include/og3/lora.h
+++ include/og3/lora.h
@@ -1,9 +1,6 @@
 // Copyright (c) 2025 Chris Lee and contibuters.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
-// Copyright (c) 2024 Chris Lee and contibuters.
-// Licensed under the MIT license. See LICENSE file in the project root for details.
-
 #pragma once
 
 #include <LoRa.h>
@@ -12,20 +9,90 @@
 #include <og3/variable.h>
 
 #include <functional>
+#include <string>
 
 namespace og3 {
+class ConfigInterface;
+
+namespace lora {
+
+enum class SpreadingFactor { kSF7, kSF8, kSF9, kSF10, kSF11, kSF12 };
+enum class SignalBandwidth { k125kHz, k500kHz };
+enum class Frequency { k433MHz, k868MHz, k915MHz };
+
+class SpreadingFactorVariable : public EnumStrVariable<SpreadingFactor> {
+ public:
+  static const char kName[];
+  static const char kDesc[];
+  SpreadingFactorVariable(const char* name, SpreadingFactor value, const char* desc, unsigned flags,
+                          VariableGroup& vg);
+  unsigned to_uint() const;
+
+  static constexpr SpreadingFactor kDefault = SpreadingFactor::kSF7;
+  static const char* kNames[6];
+};
+
+class SignalBandwidthVariable : public EnumStrVariable<SignalBandwidth> {
+ public:
+  static const char kName[];
+  static const char kDesc[];
+  SignalBandwidthVariable(const char* name, SignalBandwidth value, const char* desc, unsigned flags,
+                          VariableGroup& vg);
+  unsigned to_uint() const;
+  static constexpr SignalBandwidth kDefault = SignalBandwidth::k125kHz;
+  static const char* kNames[2];
+};
+
+class FrequencyVariable : public EnumStrVariable<Frequency> {
+ public:
+  static const char kName[];
+  FrequencyVariable(const char* name, Frequency value, const char* desc, unsigned flags,
+                    VariableGroup& vg);
+  unsigned to_uint() const;
+
+  static constexpr Frequency kDefault = Frequency::k915MHz;
+  static const char* kNames[3];
+};
+
+// Values to keep transmission time under 400msec.
+unsigned usa_max_payload_bytes(SpreadingFactor spreading_factor, SignalBandwidth signal_bandwidth);
+
+}  // namespace lora
 
 // LoRaModule helps automatically setup a LoRa radio module.
 class LoRaModule : public Module {
  public:
-  static constexpr unsigned kMaxInitTries = 10;
+  static const char kConfigUrl[];
+
+  enum class OptionSelect {
+    kNone = 0x0,
+    kAll = 0x1F,
+    kSyncWord = 0x1,
+    kEnableCrc = 0x2,
+    kFrequency = 0x4,
+    kSpreadingFactor = 0x8,
+    kSignalBandwidth = 0x10,
+  };
 
   struct Options {
     int gpio_ss = 5;    // ESP32 GPIO for SS
     int gpio_rst = 14;  // ESP32 GPIO for RST
     int gpio_dio0 = 2;  // ESP32 GPIO for DIO0
-    long frequency = 915 * kMillion;
     int max_setup_tries = 5;
+
+    unsigned sync_word = 0x01;  // Select something between 0-0xFF
+    bool enable_crc = false;
+
+    lora::Frequency frequency = lora::FrequencyVariable::kDefault;
+    lora::SpreadingFactor spreading_factor = lora::SpreadingFactorVariable::kDefault;
+    lora::SignalBandwidth signal_bandwidth = lora::SignalBandwidthVariable::kDefault;
+
+    // Which options are published via MQTT
+    OptionSelect publish_options = OptionSelect::kNone;
+    // Which options are saved/loaded from Flash
+    OptionSelect config_options = OptionSelect::kAll;
+    // Which options settable via web form.
+    OptionSelect settable_options = OptionSelect::kAll;
   };
 
   // on_initialized is called when LoRa.begin() has succeeded, for setting-up the module.
@@ -35,11 +102,32 @@ class LoRaModule : public Module {
   bool is_ok() const { return m_is_ok; }
 
  private:
+  void load_config();
   void setup_lora();
 
-  const Options m_options;
-  const std::function<void()> m_on_initialized;
   App* m_app;
+  SingleDependency m_dependencies;
+  VariableGroup& m_vg;
+  const std::function<void()> m_on_initialized;
+  const int m_gpio_ss;
+  const int m_gpio_rst;
+  const int m_gpio_dio0;
+  const int m_max_setup_tries;
+
+  std::string m_str_sync_word;
+  std::string m_str_enable_crc;
+  std::string m_str_frequency;
+  std::string m_str_spreading_factor;
+  std::string m_str_signal_bandwidth;
+
+  Variable<unsigned> m_sync_word;
+  BoolVariable m_enable_crc;
+  lora::FrequencyVariable m_frequency;
+  lora::SpreadingFactorVariable m_spreading_factor;
+  lora::SignalBandwidthVariable m_signal_bandwidth;
+
+  ConfigInterface* m_config = nullptr;
+
   unsigned m_init_tries = 0;
   bool m_is_ok = false;
 };
--- src/lora.cpp
+++ src/lora.cpp
@@ -1,21 +1,192 @@
 // Copyright (c) 2025 Chris Lee and contibuters.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
+#include <og3/config_interface.h>
 #include <og3/lora.h>
 
 namespace og3 {
+namespace lora {
+
+namespace {
+unsigned varFlag(const LoRaModule::Options& opts, LoRaModule::OptionSelect osel) {
+  auto check_set = [&osel](unsigned flags, LoRaModule::OptionSelect selmask, unsigned flag,
+                           bool invert = false) -> unsigned {
+    bool check = static_cast<unsigned>(osel) & static_cast<unsigned>(selmask);
+    if (invert) {
+      check = !check;
+    }
+    if (check) {
+      flags |= flag;
+    }
+    return flags;
+  };
+  unsigned flags = check_set(0x0, opts.publish_options, VariableBase::kNoPublish, true);
+  flags = check_set(flags, opts.config_options, VariableBase::kConfig);
+  return check_set(flags, opts.settable_options, VariableBase::kSettable);
+}
+}  // namespace
+
+const char* SpreadingFactorVariable::kNames[6] = {"SF7", "SF8", "SF9", "SF10", "SF11", "SF12"};
+const char* SignalBandwidthVariable::kNames[2] = {"125k", "500k"};
+const char* FrequencyVariable::kNames[3] = {"433MHz", "k868MHz", "k915MHz"};
+
+const char SpreadingFactorVariable::kName[] = "spreading_factor";
+const char SpreadingFactorVariable::kDesc[] = "spreading factor";
+
+const char SignalBandwidthVariable::kName[] = "signal_bandwidth";
+const char SignalBandwidthVariable::kDesc[] = "signal_bandwidth";
+
+const char FrequencyVariable::kName[] = "frequency";
+
+SpreadingFactorVariable::SpreadingFactorVariable(const char* name, SpreadingFactor value,
+                                                 const char* desc, unsigned flags,
+                                                 VariableGroup& vg)
+    : EnumStrVariable<SpreadingFactor>(name, value, desc, SpreadingFactor::kSF10, kNames, flags,
+                                       vg) {}
+
+SignalBandwidthVariable::SignalBandwidthVariable(const char* name, SignalBandwidth value,
+                                                 const char* desc, unsigned flags,
+                                                 VariableGroup& vg)
+    : EnumStrVariable<SignalBandwidth>(name, value, desc, SignalBandwidth::k500kHz, kNames, flags,
+                                       vg) {}
+
+FrequencyVariable::FrequencyVariable(const char* name, Frequency value, const char* desc,
+                                     unsigned flags, VariableGroup& vg)
+    : EnumStrVariable<Frequency>(name, value, desc, Frequency::k915MHz, kNames, flags, vg) {}
+
+unsigned SpreadingFactorVariable::to_uint() const {
+  switch (value()) {
+    case SpreadingFactor::kSF7:
+      return 7;
+    case SpreadingFactor::kSF8:
+      return 8;
+    case SpreadingFactor::kSF9:
+      return 9;
+    case SpreadingFactor::kSF10:
+      return 10;
+    case SpreadingFactor::kSF11:
+      return 11;
+    case SpreadingFactor::kSF12:
+      return 12;
+  }
+  return 8;
+}
+
+unsigned SignalBandwidthVariable::to_uint() const {
+  switch (value()) {
+    case SignalBandwidth::k125kHz:
+      return 125 * kThousand;
+    case SignalBandwidth::k500kHz:
+      return 500 * kThousand;
+  }
+  return 125 * kThousand;
+}
+
+unsigned FrequencyVariable::to_uint() const {
+  switch (value()) {
+    case Frequency::k433MHz:
+      return kMillion * 433;
+    case Frequency::k868MHz:
+      return kMillion * 868;
+    case Frequency::k915MHz:
+      return kMillion * 915;
+  }
+  return kMillion * 915;
+}
+
+// Values to keep transmission time under 400msec.
+// See https://www.thethingsnetwork.org/airtime-calculator/
+unsigned usa_max_payload_bytes(SpreadingFactor spreading_factor, SignalBandwidth signal_bandwidth) {
+  switch (signal_bandwidth) {
+    case SignalBandwidth::k125kHz:
+      switch (spreading_factor) {
+        case SpreadingFactor::kSF7:
+          return 242;
+        case SpreadingFactor::kSF8:
+          return 125;
+        case SpreadingFactor::kSF9:
+          return 53;
+        case SpreadingFactor::kSF10:
+          return 11;
+        case SpreadingFactor::kSF11:
+        case SpreadingFactor::kSF12:
+          break;  // too slow
+      }
+      break;
+    case SignalBandwidth::k500kHz:
+      switch (spreading_factor) {
+        case SpreadingFactor::kSF7:
+          return 222;
+        case SpreadingFactor::kSF8:
+          return 222;
+        case SpreadingFactor::kSF9:
+          return 222;
+        case SpreadingFactor::kSF10:
+          return 222;
+        case SpreadingFactor::kSF11:
+          return 109;
+        case SpreadingFactor::kSF12:
+          return 33;
+      }
+      break;
+  }
+  return 0;
+}
+
+#if 0
+Variable<unsigned> s_lora_sync_word("sync_word", 0xF3, nullptr, "sync word", kLoraVarFlags,
+                                    s_lora_vg);
+#endif
+
+}  // namespace lora
+
+const char LoRaModule::kConfigUrl[] = "/lora/update";
 
 LoRaModule::LoRaModule(const char* name, const LoRaModule::Options& options, App* app,
                        VariableGroup& vg, const std::function<void()>& on_initialized)
     : Module(name, &app->module_system()),
-      m_options(options),
+      m_app(app),
+      m_dependencies(ConfigInterface::kName),
+      m_vg(vg),
       m_on_initialized(on_initialized),
-      m_app(app) {
+      m_gpio_ss(options.gpio_ss),
+      m_gpio_rst(options.gpio_rst),
+      m_gpio_dio0(options.gpio_dio0),
+      m_max_setup_tries(options.max_setup_tries),
+      m_str_sync_word(std::string(name) + "_sync_word"),
+      m_str_enable_crc(std::string(name) + "_enable_crc"),
+      m_str_frequency(std::string(name) + "_" + lora::FrequencyVariable::kName),
+      m_str_spreading_factor(std::string(name) + "_" + lora::SpreadingFactorVariable::kName),
+      m_str_signal_bandwidth(std::string(name) + "_" + lora::SignalBandwidthVariable::kName),
+      m_sync_word(m_str_sync_word.c_str(), options.sync_word, nullptr, nullptr,
+                  lora::varFlag(options, OptionSelect::kSyncWord), vg),
+      m_enable_crc(m_str_enable_crc.c_str(), options.enable_crc, nullptr,
+                   lora::varFlag(options, OptionSelect::kEnableCrc), vg),
+      m_frequency(m_str_frequency.c_str(), options.frequency, nullptr,
+                  lora::varFlag(options, OptionSelect::kFrequency), vg),
+      m_spreading_factor(m_str_spreading_factor.c_str(), options.spreading_factor, nullptr,
+                         lora::varFlag(options, OptionSelect::kSpreadingFactor), vg),
+      m_signal_bandwidth(m_str_signal_bandwidth.c_str(), options.signal_bandwidth, nullptr,
+                         lora::varFlag(options, OptionSelect::kSignalBandwidth), vg) {
+  setDependencies(&m_dependencies);
+  add_link_fn([this](og3::NameToModule& name_to_module) -> bool {
+    m_config = ConfigInterface::get(name_to_module);
+    return m_config != nullptr;
+  });
   add_init_fn([this]() {
+    if (m_config) {
+      m_config->read_config(m_vg);
+    }
     // setup LoRa transceiver module
     m_app->log().debug("Calling LoRa.setPins().");
-    LoRa.setPins(m_options.gpio_ss, m_options.gpio_rst, m_options.gpio_dio0);
+    LoRa.setPins(m_gpio_ss, m_gpio_rst, m_gpio_dio0);
     if (m_on_initialized) {
+      LoRa.setSpreadingFactor(m_spreading_factor.to_uint());
+      LoRa.setSignalBandwidth(m_signal_bandwidth.to_uint());
+      LoRa.setSyncWord(m_sync_word.value());
+      if (m_enable_crc.value()) {
+        LoRa.enableCrc();
+      }
       m_app->log().debug("Calling LoRaModule on_initialized().");
       m_on_initialized();
       m_app->log().debug("LoRaModule on_initialized() finished.");
@@ -24,13 +195,27 @@ LoRaModule::LoRaModule(const char* name, const LoRaModule::Options& options, App
   add_start_fn([this]() { this->setup_lora(); });
 }
 
+void LoRaModule::load_config() {
+  if (!m_config || !m_on_initialized) {
+    return;
+  }
+  LoRa.setFrequency(m_frequency.to_uint());
+  LoRa.setSpreadingFactor(m_spreading_factor.to_uint());
+  LoRa.setSignalBandwidth(m_signal_bandwidth.to_uint());
+  LoRa.setSyncWord(m_sync_word.value());
+  if (m_enable_crc.value()) {
+    LoRa.enableCrc();
+  } else {
+    LoRa.disableCrc();
+  }
+}
+
 void LoRaModule::setup_lora() {
   m_init_tries += 1;
-  m_app->log().debugf("Calling LoRa.begin() (try %u/%u).", m_init_tries, m_options.max_setup_tries);
-  if (!LoRa.begin(m_options.frequency)) {
-    m_app->log().logf("Failed to setup LoRa: %u/%u tries.", m_init_tries,
-                      m_options.max_setup_tries);
-    if (m_init_tries < m_options.max_setup_tries) {
+  m_app->log().debugf("Calling LoRa.begin() (try %u/%u).", m_init_tries, m_max_setup_tries);
+  if (!LoRa.begin(m_frequency.to_uint())) {
+    m_app->log().logf("Failed to setup LoRa: %u/%u tries.", m_init_tries, m_max_setup_tries);
+    if (m_init_tries < m_max_setup_tries) {
       m_app->tasks().runIn(500, [this]() { setup_lora(); });
     } else {
       m_app->log().logf("Failed to setup LoRa.");
